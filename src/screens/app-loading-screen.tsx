f209e22822f4d49bcdfced8e9aedaa04aa03ada5
m9993 <muntasiralam9993@gmail.com>
2024-11-06 15:19:08 +0600

    animated splash screen

--- src/screens/app-loading-screen.tsx
+++ src/screens/app-loading-screen.tsx
@@ -1,8 +1,11 @@
 import { useAppNavigation } from '@navigation';
 import { getCurrentLocation } from '@services';
-import { colors } from '@theme';
+import { colors, textStyles } from '@theme';
 import { LinearGradient } from 'expo-linear-gradient';
 import React, { useCallback, useEffect } from 'react';
+import { Text } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { Logo } from 'src/assets/svg/logo';
 import ScreenWrapperComponent from 'src/components/wrappers/screen-wraper-component';
 import { useAppDispatch } from 'src/store/hooks';
 import { setCurrentLocation } from 'src/store/slices/app-slice';
@@ -33,7 +36,22 @@ export default function AppLoadingScreen() {
 
   return (
     <ScreenWrapperComponent statusBar={false} scrollable={false} safeAreaView={false}>
-      <LinearGradient colors={colors.light.linear} style={{ flex: 1 }}></LinearGradient>
+      <LinearGradient colors={colors.light.linear} style={{ flex: 1 }}>
+        <Animatable.View
+          style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}
+          animation="pulse"
+          easing="ease-out"
+          iterationCount="infinite">
+          <Logo width={150} height={150} />
+        </Animatable.View>
+        <Text
+          style={[
+            textStyles.poppinsRegular14,
+            { color: colors.light.grey2, textAlign: 'center', marginBottom: 30 },
+          ]}>
+          Please wait...
+        </Text>
+      </LinearGradient>
     </ScreenWrapperComponent>
   );
 }
